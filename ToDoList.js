f8d57d08b4361c13c6bd70e0a17dd0b30694721c
DataCodeLiteracy <dataliteracy@icloud.com>
2023-03-07 13:05:35 +0900

    change createContents Function

--- ToDoList.js
+++ ToDoList.js
@@ -1,43 +1,36 @@
 let id = 0;
 
-let todos = [
-  {
-    id: 0,
-    isCompleted: false,
-    content: "HTTP 공부하기"
-  },
-  {
-    id: 1,
-    isCompleted: false,
-    content: "ToDoList Project"
-  },
-  {
-    id: 2,
-    isCompleted: false,
-    content: "깃허브 공부하기"
-  }
-];
+let todos = [];
 
-const createContents = () => {
+const createTodoItem = (id, isCompleted, content) => {
   const $li = document.createElement("li");
   $li.dataset.id = id;
-  id++;
+  $li.dataset.isCompleted = isCompleted;
   $li.innerHTML = `
-    <label class="txt">${addItemInput.value}</label>
-    <input class="listItemInput" type="text" value="" />
-      <div class="item-icons">
-        <a href="#" class="complete-item mx-2 item-icon">
-          <i class="far fa-check-circle"></i>
-        </a>
-        <a href="#" class="edit-item mx-2 item-icon">
-          <i class="far fa-edit"></i>
-        </a>
-        <a href="#" class="delete-item item-icon">
-          <i class="far fa-times-circle"></i>
-        </a>
-      </div>
+  <label class="txt">${content}</label>
+  <input class="listItemInput" type="text" value="" />
+  <div class="item-icons">
+  <a href="#" class="complete-item mx-2 item-icon">
+  <i class="far fa-check-circle"></i>
+  </a>
+  <a href="#" class="edit-item mx-2 item-icon">
+  <i class="far fa-edit"></i>
+  </a>
+  <a href="#" class="delete-item item-icon">
+  <i class="far fa-times-circle"></i>
+  </a>
+  </div>
   `;
-  todos.push({ id: id, isCompleted: false, content: addItemInput.value });
-  addItemInput.value = "";
   return $li;
+};
+
+const createContents = () => {
+  const content = addItemInput.value;
+  if (content.trim().length < 1) {
+    alert("해야할 일을 입력해주세요!");
+    return null;
+  }
+  const listItem = createTodoItem(id++, false, content);
+  addItemInput.value = "";
+  return listItem;
 };
