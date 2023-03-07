d0efb4862bd2b8a092ac8b94cd5db57cf1b453ff
DataCodeLiteracy <dataliteracy@icloud.com>
2023-03-07 14:05:05 +0900

    changes completeCnts Function

--- ToDoList.js
+++ ToDoList.js
@@ -3,61 +3,36 @@ const completedItemsList = document.querySelector(".completed-itemsList");
 
 let done = [];
 
-const liRemoveArrFilterSetLocalApnChild = (
-  liList,
-  li,
-  arr,
-  arr2,
-  id,
-  key,
-  key2
-) => {
-  arr2.push({
+const liPushArr = (arr, li) => {
+  arr.push({
     id: li.dataset.id,
     isCompleted: true,
     content: li.children[0].textContent
   });
-  setLocalStorage(key2, arr2);
-  li.remove();
-  liList.appendChild(li);
-  arr = arr.filter((todo) => Number(todo.id) !== id);
-  setLocalStorage(key, arr);
 };
 
-const todoCompleted = () => {
-  todoItemsList.addEventListener("click", function (e) {
-    const target = e.target;
-    if (target.className === "far fa-check-circle") {
-      const li = target.closest("li");
-      const todoId = Number(li.dataset.id);
-      liRemoveArrFilterSetLocalApnChild(
-        completedItemsList,
-        li,
-        todos,
-        done,
-        todoId,
-        "todos",
-        "done"
-      );
-    }
-  });
+const todoCompletedCnts = (target) => {
+  if (target.className === "far fa-check-circle") {
+    const li = target.closest("li");
+    const todoId = Number(li.dataset.id);
+    liPushArr(done, li);
+    setLocalStorage("done", done);
+    li.remove();
+    completedItemsList.appendChild(li);
+    todos = todos.filter((todo) => Number(todo.id) !== todoId);
+    setLocalStorage("todos", todos);
+  }
 };
 
-const todoInCompleted = () => {
-  completedItemsList.addEventListener("click", function (e) {
-    const target = e.target;
-    if (target.className === "far fa-check-circle") {
-      const li = target.closest("li");
-      const complId = Number(li.dataset.id);
-      liRemoveArrFilterSetLocalApnChild(
-        todoItemsList,
-        li,
-        done,
-        todos,
-        complId,
-        "done",
-        "todos"
-      );
-    }
-  });
+const todoInCompletedCnts = (target) => {
+  if (target.className === "far fa-check-circle") {
+    const li = target.closest("li");
+    const complId = Number(li.dataset.id);
+    liPushArr(todos, li);
+    setLocalStorage("todos", todos);
+    li.remove();
+    todoItemsList.appendChild(li);
+    done = done.filter((compl) => Number(compl.id) !== complId);
+    setLocalStorage("done", done);
+  }
 };
