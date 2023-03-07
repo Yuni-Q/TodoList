095c682b196e0914c9d78bdd2573f0649c3dfad7
DataCodeLiteracy <dataliteracy@icloud.com>
2023-03-07 13:55:18 +0900

    change init Function

--- ToDoList.js
+++ ToDoList.js
@@ -1,11 +1,17 @@
 const init = () => {
   todos = getLocalStorage();
-  if (todos.length === 0) {
-    return;
-  } else {
-    id = todos[todos.length - 1].id + 1;
-    return todos;
-  }
+  if (todos.length === 0) return;
+  id = todos[todos.length - 1].id + 1;
+  todos.forEach((todo) => {
+    // 이미 추가된 li 요소가 있는지 확인
+    const existingListItem = document.querySelector(`li[data-id="${todo.id}"]`);
+    if (existingListItem) {
+      return; // 이미 추가된 경우, 다음으로 넘어감
+    }
+    const listItem = createTodoItem(todo.id, todo.isCompleted, todo.content);
+    todoItemsList.appendChild(listItem);
+    console.log(todos);
+  });
 };
 
 init();
