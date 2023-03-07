81441e2e099576f95f62b776375ce48734f0cc0e
DataCodeLiteracy <dataliteracy@icloud.com>
2023-03-07 14:22:48 +0900

    add JSDoc

--- ToDoList.js
+++ ToDoList.js
@@ -3,6 +3,11 @@ const completedItemsList = document.querySelector(".completed-itemsList");
 
 let done = [];
 
+/**
+ * @description 반복되는 push 동작을 함수로 구현했다.
+ * @param {Array} arr
+ * @param {Object} li
+ */
 const liPushArr = (arr, li) => {
   arr.push({
     id: li.dataset.id,
@@ -11,6 +16,10 @@ const liPushArr = (arr, li) => {
   });
 };
 
+/**
+ * @description todoItemsList에 있는 li요소의 완료버튼에 이벤트 적용 후 실행시 completedItemsList로 li요소가 이동한다.
+ * @param {Object} target
+ */
 const todoCompletedCnts = (target) => {
   if (target.className === "far fa-check-circle") {
     const li = target.closest("li");
@@ -18,12 +27,16 @@ const todoCompletedCnts = (target) => {
     liPushArr(done, li);
     setLocalStorage("done", done);
     li.remove();
-    completedItemsList.appendChild(li);
+    initVar.completedItemsList.appendChild(li);
     todos = todos.filter((todo) => Number(todo.id) !== todoId);
     setLocalStorage("todos", todos);
   }
 };
 
+/**
+ * @description completedItemsList로 있는 li요소의 완료버튼에 이벤트 적용 후 실행시 todoItemsList에 li요소가 이동한다.
+ * @param {Object} target
+ */
 const todoInCompletedCnts = (target) => {
   if (target.className === "far fa-check-circle") {
     const li = target.closest("li");
@@ -31,8 +44,11 @@ const todoInCompletedCnts = (target) => {
     liPushArr(todos, li);
     setLocalStorage("todos", todos);
     li.remove();
-    todoItemsList.appendChild(li);
+    initVar.todoItemsList.appendChild(li);
     done = done.filter((compl) => Number(compl.id) !== complId);
     setLocalStorage("done", done);
   }
 };
+
+clickEvent(initVar.todoItemsList, todoCompletedCnts);
+clickEvent(initVar.completedItemsList, todoInCompletedCnts);
