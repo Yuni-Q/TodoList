4086f4a74cbe5535387dbca89846e789414752c5
DataCodeLiteracy <dataliteracy@icloud.com>
2023-03-07 14:21:22 +0900

    add JSDoc

--- ToDoList.js
+++ ToDoList.js
@@ -8,11 +8,15 @@ const clickEvent = (elem, eventHandler) => {
   });
 };
 
+/**
+ * @description li요소들을 삭제버튼을 통해 삭제한다. 이때, todos와 done 배열에서도 해당 li요소의 id와 같은 객체를 pop해준다.
+ * @param {Object} target
+ */
 const deleteCnts = (target) => {
-  if (target.classList.contains("fa-times-circle")) {
+  if (target.className === "far fa-times-circle") {
     const li = target.closest("li");
     const ItemsListId = Number(li.dataset.id);
-    if (li.parentNode === todoItemsList) {
+    if (li.parentNode === initVar.todoItemsList) {
       li.remove();
       todos = todos.filter((todo) => Number(todo.id) !== ItemsListId);
       setLocalStorage("todos", todos);
@@ -23,3 +27,6 @@ const deleteCnts = (target) => {
     }
   }
 };
+
+clickEvent(initVar.todoItemsList, deleteCnts);
+clickEvent(initVar.completedItemsList, deleteCnts);
