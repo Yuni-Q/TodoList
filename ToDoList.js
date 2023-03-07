f58e26e773b45c2190e3f5bff7052c42ac70d771
DataCodeLiteracy <dataliteracy@icloud.com>
2023-03-07 14:00:31 +0900

    delete Contents add Click Event

--- ToDoList.js
+++ ToDoList.js
@@ -1,10 +1,12 @@
-const deleteItem = (e) => {
-  const target = e.target;
-  if (target.classList.contains("delete-item")) {
-    const li = target.closest("li");
-    const todoId = Number(li.dataset.id);
-    todos = todos.filter((todo) => todo.id !== todoId);
-    setLocalStorage();
-    li.remove();
-  }
+const deleteItem = (itemsList) => {
+  itemsList.addEventListener("click", function (e) {
+    const target = e.target;
+    if (target.classList.contains("fa-times-circle")) {
+      const li = target.closest("li");
+      const todoId = Number(li.dataset.id);
+      todos = todos.filter((todo) => todo.id !== todoId);
+      setLocalStorage();
+      li.remove();
+    }
+  });
 };
