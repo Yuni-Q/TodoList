ad2e2be53f4edfaca4834521839f00e2ba2d46f3
DataCodeLiteracy <dataliteracy@icloud.com>
2023-03-07 13:57:17 +0900

    inif Function Error Handling

--- ToDoList.js
+++ ToDoList.js
@@ -1,17 +1,62 @@
 const init = () => {
-  todos = getLocalStorage();
-  if (todos.length === 0) return;
-  id = todos[todos.length - 1].id + 1;
-  todos.forEach((todo) => {
-    // 이미 추가된 li 요소가 있는지 확인
-    const existingListItem = document.querySelector(`li[data-id="${todo.id}"]`);
-    if (existingListItem) {
-      return; // 이미 추가된 경우, 다음으로 넘어감
-    }
-    const listItem = createTodoItem(todo.id, todo.isCompleted, todo.content);
-    todoItemsList.appendChild(listItem);
-    console.log(todos);
-  });
+  todos = getLocalStorage("todos");
+  done = getLocalStorage("done");
+  if (todos.length === 0 && done.length === 0) {
+    id = 0;
+    return;
+  }
+  if (todos.length > 0 && done.length === 0) {
+    id = Number(todos[todos.length - 1].id) + 1;
+    todos.forEach((todo) => {
+      const existingListItem = document.querySelector(
+        `li[data-id="${todo.id}"]`
+      );
+      if (existingListItem) {
+        return; // 이미 추가된 경우, 다음으로 넘어감
+      }
+      const listItem = createTodoItem(todo.id, todo.isCompleted, todo.content);
+      todoItemsList.appendChild(listItem);
+    });
+  }
+  if (done.length > 0 && todos.length === 0) {
+    id = Number(done[done.length - 1].id) + 1;
+    done.forEach((compl) => {
+      const existingListItem = document.querySelector(
+        `li[data-id="${compl.id}"]`
+      );
+      if (existingListItem) return;
+      const listItem = createTodoItem(
+        compl.id,
+        compl.isCompleted,
+        compl.content
+      );
+      completedItemsList.appendChild(listItem);
+    });
+  } else {
+    id = Number(todos[todos.length - 1].id) + 1;
+    todos.forEach((todo) => {
+      const existingListItem = document.querySelector(
+        `li[data-id="${todo.id}"]`
+      );
+      if (existingListItem) {
+        return;
+      }
+      const listItem = createTodoItem(todo.id, todo.isCompleted, todo.content);
+      todoItemsList.appendChild(listItem);
+    });
+    done.forEach((compl) => {
+      const existingListItem = document.querySelector(
+        `li[data-id="${compl.id}"]`
+      );
+      if (existingListItem) return;
+      const listItem = createTodoItem(
+        compl.id,
+        compl.isCompleted,
+        compl.content
+      );
+      completedItemsList.appendChild(listItem);
+    });
+  }
 };
 
 init();
