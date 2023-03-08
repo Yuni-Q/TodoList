766f67ddf4ac0dce87d1f9b1e6dbb74cb356cd66
DataCodeLiteracy <dataliteracy@icloud.com>
2023-03-09 03:07:27 +0900

    JS - fix localStorage JSDoc

--- ToDoList.js
+++ ToDoList.js
@@ -1,15 +1,13 @@
 /**
- * @description 배열 요소들을 localStorage에 JSON 형식으로 저장한다.
- * @param {string} key localStorage에 저장할 key 이름
- * @param {array} arr localStorage에 저장할 배열
+ * 배열 요소들을 localStorage에 JSON 형식으로 저장한다.
  */
 const setLocalStorage = () => {
   localStorage.setItem("todos", JSON.stringify(todos));
 };
 
 /**
- * @description localStorage에 있는 key 항목을 불러온 후 JSON 형식으로 파싱하여 반환한다. key가 존재하지 않으면 빈 배열을 반환한다.
- * @param {string} key localStorage에서 불러올 key 이름
+ * localStorage에 있는 key 항목을 불러온 후 JSON 형식으로 파싱하여 반환한다. key가 존재하지 않으면 빈 배열을 반환한다.
+ * @returns {Array} todos - localStorage에서 가져온 JSON 형식의 todos 배열 또는 빈 배열.
  */
 const getLocalStorage = () => {
   return JSON.parse(localStorage.getItem("todos")) || [];
