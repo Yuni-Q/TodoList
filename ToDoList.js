5a2532a30aeff70a09d29092fa18fb54403529de
DataCodeLiteracy <dataliteracy@icloud.com>
2023-03-09 05:55:13 +0900

    init Function fix duplication

--- ToDoList.js
+++ ToDoList.js
@@ -1,7 +1,19 @@
+function bindEventHandlers() {
+  clickEvent(initVar.addBtn, addCnts);
+  clickEvent(initVar.todoItemsList, deleteCnts);
+  clickEvent(initVar.completedItemsList, deleteCnts);
+  clickEvent(initVar.todoItemsList, todoCompletedCnts);
+  clickEvent(initVar.completedItemsList, todoInCompletedCnts);
+  clickEvent(initVar.container, editCntsClick);
+  editCntsEnter();
+}
+
 /**
  * 최초 웹페이지 로드될 떄와 새로고침이 발생할 때, localStorage에 저장되어 있는 파일을 불러와 그 당시 배열에 저장되어 있는 li요소들을 렌더링한다.
  */
+
 const init = () => {
+  bindEventHandlers();
   todos = getLocalStorage("todos");
   done = getLocalStorage("done");
   if (todos.length === 0 && done.length === 0) {
@@ -18,24 +30,21 @@ const init = () => {
       `li[data-id="${itemId}"]`
     );
     if (existingTodoListItem) return;
-    const todoItem = todos.find((todo) => todo.id === itemId);
-    if (todoItem) {
-      const listItem = createTodoItem(
-        todoItem.id,
-        todoItem.isCompleted,
-        todoItem.content
+    const item =
+      todos.find((todo) => todo.id === itemId) ||
+      done.find((compl) => compl.id === itemId);
+    if (item) {
+      const contents = new initVar.Contents(
+        item.id,
+        item.isCompleted,
+        item.content
       );
-      todoItemsList.appendChild(listItem);
-      return;
-    }
-    const doneItem = done.find((compl) => compl.id === itemId);
-    if (doneItem) {
-      const listItem = createTodoItem(
-        doneItem.id,
-        doneItem.isCompleted,
-        doneItem.content
-      );
-      completedItemsList.appendChild(listItem);
+      const listItem = contents.createTodoItem();
+      if (item.isCompleted) {
+        initVar.completedItemsList.appendChild(listItem);
+      } else {
+        initVar.todoItemsList.appendChild(listItem);
+      }
     }
   });
 };
