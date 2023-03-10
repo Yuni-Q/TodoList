004bcab59ec4954b6f7c4eb60a5d27d310acc4cb
DataCodeLiteracy <dataliteracy@icloud.com>
2023-03-10 13:43:55 +0900

    fix createContents Function

--- ToDoList.js
+++ ToDoList.js
@@ -14,19 +14,19 @@ const createTodoItem = (id, isCompleted, content) => {
   $li.dataset.id = id;
   $li.dataset.isCompleted = isCompleted;
   $li.innerHTML = `
-  <label class="txt">${content}</label>
-  <input class="listItemInput" type="text" value="" />
-  <div class="item-icons">
-  <a href="#" class="complete-item mx-2 item-icon">
-  <i class="far fa-check-circle"></i>
-  </a>
-  <a href="#" class="edit-item mx-2 item-icon">
-  <i class="far fa-edit"></i>
-  </a>
-  <a href="#" class="delete-item item-icon">
-  <i class="far fa-times-circle"></i>
-  </a>
-  </div>
+    <label class="txt">${content}</label>
+    <input class="listItemInput" type="text" value="" />
+    <div class="item-icons">
+      <a href="#" class="complete-item mx-2 item-icon">
+        <i class="far fa-check-circle"></i>
+      </a>
+      <a href="#" class="edit-item mx-2 item-icon">
+        <i class="far fa-edit"></i>
+      </a>
+      <a href="#" class="delete-item item-icon">
+        <i class="far fa-times-circle"></i>
+      </a>
+    </div>
   `;
   return $li;
 };
