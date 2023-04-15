51de6f8cf04f47e149ba380a621facf8aade489e
SoohoPark <clapp301@gmail.com>
2023-04-15 17:50:15 +0900

    chore: ts 초기설정

--- src/js/TodoList.ts
+++ src/js/TodoList.ts
@@ -1,4 +1,4 @@
-import Validate from './Validation.js';
+import Validate from './Validation';
 
 /**
  *
--- src/js/main.ts
+++ src/js/main.ts
@@ -1,4 +1,4 @@
-import App from './App.js';
+import App from './App';
 
 const $target = document.querySelector('#todo-list');
 
